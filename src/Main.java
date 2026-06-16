0ee5a18f77c8522b3642c6a8f8c9f6e084f33b66
Slava <titovve@mail.ru>
2026-06-16 21:24:40 +0300

    Added output with two languages

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import static java.lang.System.out;
 
 public class Main {
     public static void main(String[] args) {
-        out.print("Привет и добро пожаловать!");
+        out.println("Привет и добро пожаловать!");
+        out.println("Hello and welcome!");
         for (int i = 1; i <= 5; i++) {
             out.println("i = " + i);
         }
