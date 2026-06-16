4d2348ba41ff7c93903c6589081066ea0e4518d2
Slava <titovve@mail.ru>
2026-06-16 06:44:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) {
+        out.print("Hello and welcome!");
+        for (int i = 1; i <= 5; i++) {
+            out.println("i = " + i);
+        }
+    }
+}
