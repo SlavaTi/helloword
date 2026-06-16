9f10c218cc4274bd6477214f073c20d81602aede
Slava <titovve@mail.ru>
2026-06-16 06:53:40 +0300

    Message сhanged to "Привет и добро пожаловать!"

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import static java.lang.System.out;
 
 public class Main {
     public static void main(String[] args) {
-        out.print("Hello and welcome!");
+        out.print("Привет и добро пожаловать!");
         for (int i = 1; i <= 5; i++) {
             out.println("i = " + i);
         }
